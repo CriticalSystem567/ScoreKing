d9a2cce41b0af409d9d9bdca86400e27ce04957a
CriticalSystem567 <kethaeswar01@gmail.com>
2026-07-03 15:29:39 +0530

    Update supabaseClient.js

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,6 +1,4 @@
 import { createClient } from "@supabase/supabase-js";
-
-const SUPABASE_URL = "https://hcsdtmzyozrvbzihudxf.supabase.co";
-const SUPABASE_KEY = "sb_publishable_w4kLcs3_-cmBFUAywA5zyw_JI-3zB5J";
-
+const SUPABASE_URL = "https://hbsshniezcdsmwkbidzz.supabase.co";
+const SUPABASE_KEY = "sb_publishable_wxgXoNcuKtrn9CWDLxI-bA_sV8L9i2s";
 export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
